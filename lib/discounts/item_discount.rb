1a2d99cbbfd91fa55ab769f530822ea478f1191f
michaellennox <michaelctlennox@gmail.com>
2016-03-21 13:39:25 +0000

    Passes unit test for applying an item discount

--- lib/discounts/item_discount.rb
+++ lib/discounts/item_discount.rb
@@ -1,3 +1,15 @@
 class ItemDiscount
+  def initialize(item_code, min_items, discount_to_apply)
+    @item_code = item_code
+    @min_items = min_items
+    @discount_to_apply = discount_to_apply
+  end
 
+  def apply(current_total, order)
+    order[item_code] > min_items ? order[item_code] * discount_to_apply : 0
+  end
+
+  private
+
+  attr_reader :item_code, :min_items, :discount_to_apply
 end
