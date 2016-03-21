fdcc10bcc26ecc89b7fcb8990de41a08cb9ff8df
michaellennox <michaelctlennox@gmail.com>
2016-03-21 13:49:11 +0000

    Extracts logic to private methods for PercentDiscount

--- lib/discounts/percent_discount.rb
+++ lib/discounts/percent_discount.rb
@@ -5,10 +5,18 @@ class PercentDiscount
   end
 
   def apply(current_total, order)
-    current_total > min_value ? current_total * percent_discount / 100 : 0
+    total_enough_for_discount?(current_total) ? apply_discount(current_total) : 0
   end
 
   private
 
   attr_reader :percent_discount, :min_value
+
+  def total_enough_for_discount?(current_total)
+    current_total > min_value
+  end
+
+  def apply_discount(current_total)
+    current_total * percent_discount / 100
+  end
 end
