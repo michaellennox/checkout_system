2e5afc2e67de52befc68f0c25bdf707edcdf1b63
michaellennox <michaelctlennox@gmail.com>
2016-03-21 13:30:04 +0000

    Passes unit test for applying percent discount

--- lib/discounts/percent_discount.rb
+++ lib/discounts/percent_discount.rb
@@ -1,3 +1,14 @@
 class PercentDiscount
+  def initialize(percent_discount, min_value)
+    @percent_discount = percent_discount
+    @min_value = min_value
+  end
 
+  def apply(current_total, order)
+    current_total > min_value ? current_total * percent_discount / 100 : 0
+  end
+
+  private
+
+  attr_reader :percent_discount, :min_value
 end
